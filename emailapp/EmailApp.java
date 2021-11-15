28a9141b04eaef0afa10007aa97457f5db908433
Manoj Thapa <83998458+mzmagar@users.noreply.github.com>
2021-11-15 00:07:45 -0600

    Add files via upload

---
+++ emailapp/EmailApp.java
@@ -0,0 +1,14 @@
+package emailapp;
+
+public class EmailApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Email em1 = new Email("Bibek","Laudari");
+		System.out.println(em1.showInfo());
+		
+	//	em1.setAlternateEmail("sg@ymail.com");
+	//	System.out.println(em1.getAlternateEmail());
+	}
+
+}
